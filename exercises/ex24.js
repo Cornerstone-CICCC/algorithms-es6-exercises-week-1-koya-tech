189a1c51aa7794fc1437c75a78244d39434db758
koya <koya9431ptr@gmail.com>
2024-09-19 16:37:49 +0000

    exercise 24 completed

--- exercises/ex24.js
+++ exercises/ex24.js
@@ -20,3 +20,20 @@ console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
 console.log(lastIndexOf([], 3), "=?", -1);
 
 */
+
+function lastIndexOf(arr, num) {
+    let output = -1
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] === num) {
+            output = i;
+            return output;
+        }
+    }
+    return output;
+}
+
+console.log(lastIndexOf([0, 1, 4, 1, 2], 1), "=?", 3);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 2), "=?", 4);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 3), "=?", -1);
+console.log(lastIndexOf([5, 5, 5], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
