71f880458b31ef0c52ee0a145ca2887c9332cb98
koya <koya9431ptr@gmail.com>
2024-09-19 16:53:00 +0000

    exercise 26 completed

--- exercises/ex26.js
+++ exercises/ex26.js
@@ -5,3 +5,47 @@ The Lumberjack - $9.95
 eggs, sausage, toast, hashbrowns, pancakes
 The object should contain properties for the name, price, and ingredients.
 */
+
+class Lumberjack {
+    constructor(balabnce) {
+        this.balabnce = balabnce;
+    }
+}
+
+class Eggs {
+    constructor(name, price, ingredients) {
+        this.name = name;
+        this.price = price;
+        this.ingredients = ingredients;
+    }
+}
+
+class Sausage {
+    constructor(name, price, ingredients) {
+        this.name = name;
+        this.price = price;
+        this.ingredients = ingredients;
+    }
+}
+class Toast {
+    constructor(name, price, ingredients) {
+        this.name = name;
+        this.price = price;
+        this.ingredients = ingredients;
+    }
+}
+
+class Hashbrowns {
+    constructor(name, price, ingredients) {
+        this.name = name;
+        this.price = price;
+        this.ingredients = ingredients;
+    }
+}
+class Pancakes {
+    constructor(name, price, ingredients) {
+        this.name = name;
+        this.price = price;
+        this.ingredients = ingredients;
+    }
+}
